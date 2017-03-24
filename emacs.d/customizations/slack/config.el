f8817934d9a92517ec9a917522352e3bbea3333d
Matt Usifer <musifer@healthverity.com>
2017-03-24 14:58:34 -0400

    Reorganize config file

--- emacs.d/customizations/slack/config.el
+++ emacs.d/customizations/slack/config.el
@@ -1,3 +1,17 @@
+;; slack functions
+(defun read-secret-key (path)
+  (ignore-errors
+    (string-trim
+     (with-temp-buffer
+       (insert-file-contents path)
+       (buffer-string)))))
+
+(defun close-buffer-and-window ()
+  (interactive)
+  (kill-buffer)
+  (delete-window))
+
+;; slack config
 (use-package slack
   :commands (slack-start)
   :init
@@ -7,31 +21,18 @@
   (slack-register-team
    :name "healthverity"
    :default t
-   :client-id 
-   (ignore-errors
-     (string-trim (with-temp-buffer
-             (insert-file-contents "~/.emacs.d/customizations/slack/client-id")
-             (buffer-string))))
-   :client-secret
-   (ignore-errors
-     (string-trim (with-temp-buffer
-             (insert-file-contents "~/.emacs.d/customizations/slack/client-secret")
-             (buffer-string))))
+   :client-id (read-secret-key "~/.emacs.d/customizations/slack/client-id")
+   :client-secret (read-secret-key "~/.emacs.d/customizations/slack/client-secret")
    )
   )
-
 (use-package alert
   :commands (alert)
   :init
   (setq alert-default-style 'message))
 
+;; key bindings
 (global-set-key (kbd "C-c s g") 'slack-group-select)
 (global-set-key (kbd "C-c s c") 'slack-channel-select)
 (global-set-key (kbd "C-c s i") 'slack-im-select)
-
-(defun close-buffer-and-window ()
-  (interactive)
-  (kill-buffer)
-  (delete-window))
 
 (add-hook 'slack-mode-hook (lambda () (local-set-key (kbd "C-c C-q") 'close-buffer-and-window)))
