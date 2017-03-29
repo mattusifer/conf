06a9b86fb52128117362497630a74fe70dbc4af9
Matt Usifer <musifer@healthverity.com>
2017-03-29 14:01:11 -0400

    Add new context for personal gmail

--- emacs.d/customizations/email/config.el
+++ emacs.d/customizations/email/config.el
@@ -5,9 +5,32 @@
 (setq mu4e-mu-binary "/usr/local/bin/mu")
 (setq mu4e-compose-dont-reply-to-self t)
 
+ (setq mu4e-contexts
+    `( ,(make-mu4e-context
+          :name "Personal Gmail"
+          :enter-func (lambda () (mu4e-message "Entering Personal Gmail context"))
+          :leave-func (lambda () (mu4e-message "Leaving Personal Gmail context"))
+          ;; we match based on the contact-fields of the message
+          :match-func (lambda (msg)
+                        (when msg 
+                          (mu4e-message-contact-field-matches msg 
+                            :to "mattusifer@gmail.com")))
+          :vars '( ( user-mail-address      . "mattusifer@gmail.com"  )
+                   ( user-full-name         . "Matt Usifer" )
+                   ( mu4e-compose-signature . "- Matt")))
+       ))
 
-(setq user-mail-address "mattusifer@gmail.com")
-(setq user-full-name "Matt Usifer")
+  ;; set `mu4e-context-policy` and `mu4e-compose-policy` to tweak when mu4e should
+  ;; guess or ask the correct context, e.g.
+
+  ;; start with the first (default) context; 
+  ;; default is to ask-if-none (ask when there's no context yet, and none match)
+  ;; (setq mu4e-context-policy 'pick-first)
+
+  ;; compose with the current context is no context matches;
+  ;; default is to ask 
+  ;; (setq mu4e-compose-context-policy nil)
+
 
 ;; refresh mail dir every 2 minutes
 (defun refresh-mail ()
