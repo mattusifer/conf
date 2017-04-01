290c0d2f2d20d352d7be864c8c82c643990ff8da
Matt Usifer <mattusifer@gmail.com>
2017-04-01 15:37:34 -0400

    Fix read-secret-key function in spotify, use correct auth for gmail

--- emacs.d/settings/setup-spotify.el
+++ emacs.d/settings/setup-spotify.el
@@ -1,11 +1,11 @@
 (require 'spotify)
 
-;; slack functions
 (defun read-secret-key (path)
   (ignore-errors
     (string-trim
      (with-temp-buffer
-       (insert-file-contents path)))))
+       (insert-file-contents path)
+       (buffer-string)))))
 
 (setq spotify-oauth2-client-id
       (read-secret-key (expand-file-name "secrets/spotify-client-id" user-emacs-directory)))
