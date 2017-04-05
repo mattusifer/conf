36a2e8455dfc0197c552abdefa3d4eb4d94e0854
Matt Usifer <musifer@healthverity.com>
2017-04-04 23:19:55 -0400

    Use send-to-process in scala repl

--- emacs.d/settings/setup-scala.el
+++ emacs.d/settings/setup-scala.el
@@ -1,32 +1,41 @@
 (require 'ensime)
+(require 'send-to-process)
 (setq ensime-startup-notification nil)
 
 (defun get-last-occurrence (substr string start)
+  "Get the last occurrence of a substring within a string"
   (let ((match-index (string-match substr string start)))
     (if (and match-index (string-match substr string (match-end 0)))
         (get-last-occurrence substr string (+ match-index (length substr)))
       match-index)))
 
 (defun get-base-dir (str)
+  "Get the base directory of the current scala project"
   (substring str 0 (get-last-occurrence "src" str 0)))
 
-(defun eval-scala (str dir)
+(defun eval-scala (execution-fn)
+  "
+Evaluate a scala string in a repl - will pop one open if it
+doesn't already exist
+"
   ;; start scala if it hasn't started yet
   (unless (get-process "scala-repl")
 
     ;; look for a 'src' folder in the current dir
     ;; if none exists, get the base dir
     (if (not (member "src" (directory-files default-directory)))
-        (setq default-directory (get-base-dir dir)))
+        (setq default-directory (get-base-dir default-directory)))
     
     (let ((process-connection-type nil))  ; use a pipe
       (start-process "scala-repl" "*scala*"  "sbt" "-J-Xms3G" "-J-Xmx6G" "compile" "console"))
     (set-buffer "*scala*")
     (linum-mode -1)
     (special-mode)
-    (scala-mode))  
+    (scala-mode))
+
   ;; execute
-  (process-send-string "scala-repl" str)
+  (funcall execution-fn)
+
   ;;display buffer
   (unless (get-buffer-window "*scala*")
     (display-buffer 
@@ -38,20 +47,11 @@
 (defun eval-scala-buffer ()
   "will evaluate entire buffer"
   (interactive)
-  (if (use-region-p)
-      (progn (eval-scala
-              (replace-regexp-in-string "\n\s*\n" "\n" (buffer-substring-no-properties (region-beginning) (region-end))) default-directory)
-             (eval-scala "\n" default-directory))
-    (eval-scala (replace-regexp-in-string "\n\s*\n" "\n" (buffer-string)) default-directory)))
+  (eval-scala (lambda () (send-buffer-region-to-process "scala-repl"))))
 
 (defun eval-scala-region ()
-  "will evaluate a single line or a region, depending on whether it's highlighted"
   (interactive)
-  (if (use-region-p)
-      (progn (eval-scala 
-              (replace-regexp-in-string "\n\s*" "" (buffer-substring-no-properties (region-beginning) (region-end))) default-directory) 
-             (eval-scala "\n" default-directory))
-      (eval-scala (thing-at-point 'line) default-directory)))
+  (eval-scala (lambda () (send-line-region-to-process "scala-repl"))))
 
 (add-hook 'scala-mode-hook (lambda () (local-set-key (kbd "C-c C-e") 'eval-scala-region)))
 (add-hook 'scala-mode-hook (lambda () (local-set-key (kbd "C-c C-k") 'eval-scala-buffer)))
