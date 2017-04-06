ed591bffb97cbfff3da2e79dcf59386cc24902fc
Matt Usifer <mattusifer@gmail.com>
2017-04-06 14:20:19 -0400

    Ignore errors if calendar can't be found

--- emacs.d/settings/setup-org.el
+++ emacs.d/settings/setup-org.el
@@ -12,21 +12,22 @@
        (buffer-string)))))
 
 (defun refresh-calendar (url-location)
-  (let ((ical2org (expand-file-name "bin/ical2org.awk" user-emacs-directory))
-        (ics-file (expand-file-name "hv-cal.ics" user-emacs-directory))
-        (org-file "~/Dropbox/symlinks/emacs/org-mode/calendar.org"))
-    
-    (when (file-exists-p ics-file)
-      (delete-file ics-file))
+  (ignore-errors
+    (let ((ical2org (expand-file-name "bin/ical2org.awk" user-emacs-directory))
+          (ics-file (expand-file-name "hv-cal.ics" user-emacs-directory))
+          (org-file "~/Dropbox/symlinks/emacs/org-mode/calendar.org"))
+      
+      (when (file-exists-p ics-file)
+        (delete-file ics-file))
 
-    (url-copy-file (read-secret-key url-location) ics-file)
-    (start-process-shell-command (concat url-location " calendar-refresh")  nil
-                                 (concat ical2org " < " ics-file " > " org-file))
-    (delete-file ics-file)))
+      (url-copy-file (read-secret-key url-location) ics-file)
+      (start-process-shell-command (concat url-location " calendar-refresh")  nil
+                                   (concat ical2org " < " ics-file " > " org-file))
+      (delete-file ics-file))))
 
 ;; healthverity calendar
 (run-with-timer 0 (* 60 5) 'refresh-calendar
-             (expand-file-name "secrets/hv-calendar-url" user-emacs-directory))
+                (expand-file-name "secrets/hv-calendar-url" user-emacs-directory))
 
 ;; notify appts
 (org-agenda-to-appt)
