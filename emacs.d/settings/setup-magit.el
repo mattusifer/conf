37f321f03ba9e3063d35500fbf45ec3a8e4e4fde
Matt Usifer <musifer@healthverity.com>
2017-04-12 16:40:47 -0400

    Pop open github PR for current branch

--- emacs.d/settings/setup-magit.el
+++ emacs.d/settings/setup-magit.el
@@ -39,12 +39,35 @@
             (get-url origin-url))
         (get-url upstream-url)))))
 
+(defun get-pull-request-url ()
+  "Open 'compare' github URL comparing current branch (on origin) to upstream/master"
+  (interactive)
+  (let ((upstream-url (magit-get "remote" "upstream" "url"))
+        (origin-url (magit-get "remote" "origin" "url")))
+    (flet ((get-url (upstream-url origin-url)
+                    (format "%s/compare/master...%s:%s"
+                            (replace-regexp-in-string
+                             (rx (and string-start (1+ any) "github.com:" (group (1+ any)) ".git" string-end))
+                             "https://github.com/\\1" upstream-url)
+                            (replace-regexp-in-string
+                             (rx (and string-start (1+ any) "github.com:" (group (1+ any)) "/" (group (1+ any)) ".git" string-end))
+                             "\\1" origin-url)
+                            (magit-get-current-branch))))
+      (if (and (not (null upstream-url)) (not (null origin-url)))
+          (get-url upstream-url origin-url)
+        (message "No 'upstream' or 'origin' git remote is set on this repo")))))
+
 (defun pop-open-github-repo ()
   (interactive)
   (browse-url (get-github-url)))
 
+(defun pop-open-github-pr ()
+  (interactive)
+  (browse-url (get-pull-request-url)))
+
 (global-set-key (kbd "C-c m s") 'magit-status)
 (global-set-key (kbd "C-c m p") 'magit-push)
 (global-set-key (kbd "C-c m o") 'pop-open-github-repo)
+(global-set-key (kbd "C-c m r") 'pop-open-github-pr)
 
 (provide 'setup-magit)
