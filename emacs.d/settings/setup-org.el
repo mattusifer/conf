5043f27bdf90285aba37d23430b4cd69fafd0f5d
Matt Usifer <mattusifer@gmail.com>
2019-02-05 09:18:10 -0500

    Always store relative paths

--- emacs.d/settings/setup-org.el
+++ emacs.d/settings/setup-org.el
@@ -3,6 +3,9 @@
       (mapcar (lambda (x)  (concat "~/Seafile/Org Agenda/" x))
               (-filter (lambda (x) (s-suffix? ".org" x)) (directory-files "~/Seafile/Org Agenda"))))
 
+;; always store relative paths for links
+(setq org-link-file-path-type 'relative)
+
 ;; notify appts
 (org-agenda-to-appt)
 
