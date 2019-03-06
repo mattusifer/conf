955867d318f1b26b14957bb87d955316da24269b
Matt Usifer <mattusifer@gmail.com>
2019-03-05 21:21:18 -0500

    Fix formatting on save

--- emacs.d/settings/setup-rust.el
+++ emacs.d/settings/setup-rust.el
@@ -1,14 +1,35 @@
 ;; format buffer
+(defun format-with-saved-position
+    ()
+  (let ((w-start (window-start)))
+    (rust-format-buffer)
+    (set-window-start (selected-window) w-start)))
 (add-hook 'rust-mode-hook
           (lambda ()
-            (local-set-key (kbd "C-c f") #'rust-format-buffer)))
+            (local-set-key (kbd "C-c f") #'format-with-saved-position)))
+(add-hook 'rust-mode-hook
+          (lambda ()
+             (add-hook 'before-save-hook 'format-with-saved-position nil t)))
 
 ;; racer setup
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;; racer describe
+(add-hook 'racer-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c d") #'racer-describe)))
+
+;; kbds
+(add-hook 'rust-mode-hook
+          (lambda () (local-set-key (kbd "TAB") 'company-indent-or-complete-common)))
+(add-hook 'rust-mode-hook
+          (lambda () (local-set-key (kbd "<tab>") 'company-indent-or-complete-common)))
+
+(add-hook 'rust-mode-hook
+          (lambda () (local-set-key (kbd "<tab>") 'company-indent-or-complete-common)))
+
 (setq company-tooltip-align-annotations t)
 
 ;; cargo setup
